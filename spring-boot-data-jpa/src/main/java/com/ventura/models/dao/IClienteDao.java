eb46e307295ae4da52f543687150cb64f0b6bfcd
JIMMY <JIMMY@DESKTOP-M1938FH>
2019-08-06 19:15:40 -0500

    50.-Decorando la vista listar con estilos CSS Bootstrap

---
+++ spring-boot-data-jpa/src/main/java/com/ventura/models/dao/IClienteDao.java
@@ -0,0 +1,10 @@
+package com.ventura.models.dao;
+
+import java.util.List;
+
+import com.ventura.models.entity.Cliente;
+
+public interface IClienteDao {
+
+	public List<Cliente> findAll();
+}
---
+++ spring-boot-data-jpa/src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO clientes(id,nombre,apellido,email, create_at)VALUES(1,'Andres','Guzman','profesor@hotmail.com','2019-08-05');
+INSERT INTO clientes(id,nombre,apellido,email, create_at)VALUES(2,'Carla','Guzman','profesor2@hotmail.com','2019-08-05');
