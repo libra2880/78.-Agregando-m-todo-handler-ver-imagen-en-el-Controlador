5fb47f624d975c6c9fad160dc684036726f01443
JIMMY <JIMMY@DESKTOP-M1938FH>
2019-08-08 05:29:46 -0500

    56. Agregando reglas de validación usando anotaciones

--- spring-boot-data-jpa/src/main/java/com/ventura/models/entity/Cliente.java
+++ spring-boot-data-jpa/src/main/java/com/ventura/models/entity/Cliente.java
@@ -12,6 +12,9 @@ import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -25,12 +28,17 @@ public class Cliente implements Serializable{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;	
 	
+	@NotEmpty
 	private String nombre;
 	
+	@NotEmpty
 	private String apellido;
 	
+	@NotEmpty
+	@Email
 	private String email;
 	
+	@NotNull
 	@Column(name="create_at")
 	@Temporal(TemporalType.DATE)
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
