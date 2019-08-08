3c86a93c36d269f8b122ef92286ff91123bf66f1
JIMMY <JIMMY@DESKTOP-M1938FH>
2019-08-07 19:06:36 -0500

    55. Agregando campo fecha en formulario

--- spring-boot-data-jpa/src/main/java/com/ventura/models/entity/Cliente.java
+++ spring-boot-data-jpa/src/main/java/com/ventura/models/entity/Cliente.java
@@ -13,6 +13,8 @@ import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 @Entity
 @Table(name="clientes")
 public class Cliente implements Serializable{
@@ -31,12 +33,10 @@ public class Cliente implements Serializable{
 	
 	@Column(name="create_at")
 	@Temporal(TemporalType.DATE)
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date createAt;
 	
-	@PrePersist
-	public void prePersist() {
-		createAt=new Date();
-	}
+
 
 	public Long getId() {
 		return id;
